b3f6cbc90d4e3bf3bfe8249b110fc2d0c4fe07cb
Alexander Couzens <lynxis@fe80.eu>
2017-08-02 06:28:13 +0200

    remove port 0
    Change-Id: Ieff8055a6d23602e689c10d34c0f0657830bacdf

--- src/mainboard/hp/proliant_microserver_gen8/early_southbridge.c
+++ src/mainboard/hp/proliant_microserver_gen8/early_southbridge.c
@@ -66,8 +66,6 @@ void mainboard_config_superio(void)
 /* FIXME: Put proper SPD map here. */
 void mainboard_get_spd(spd_raw_data *spd, bool id_only)
 {
-	read_spd(&spd[0], 0x50, id_only);
 	read_spd(&spd[1], 0x51, id_only);
-	read_spd(&spd[2], 0x52, id_only);
 	read_spd(&spd[3], 0x53, id_only);
 }
